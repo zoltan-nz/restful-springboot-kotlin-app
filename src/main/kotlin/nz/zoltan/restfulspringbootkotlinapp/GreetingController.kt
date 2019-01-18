e8a17079ba41c9d26dc1e6c492e8e7081e403970
Zoltan <zdebre@gmail.com>
2019-01-19 10:52:00 +1300

    Add greeting endpoint

---
+++ src/main/kotlin/nz/zoltan/restfulspringbootkotlinapp/GreetingController.kt
@@ -0,0 +1,18 @@
+package nz.zoltan.restfulspringbootkotlinapp
+
+import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.RequestParam
+import org.springframework.web.bind.annotation.RestController
+import java.util.concurrent.atomic.AtomicLong
+
+data class Greeting(val id: Long, val content: String)
+
+@RestController
+class GreetingController {
+    val counter = AtomicLong()
+
+    @GetMapping("/greeting")
+    fun greeting(@RequestParam(value = "name", defaultValue = "World") name: String) =
+            Greeting(counter.incrementAndGet(), "Hello, $name")
+
+}
